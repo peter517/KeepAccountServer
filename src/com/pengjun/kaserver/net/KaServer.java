2a23042df900877098434581f0451b30a5298e70
PenGuin <peter517@126.com>
2013-09-12 20:57:10 +0800

    refactor

--- src/com/pengjun/kaserver/net/KaServer.java
+++ src/com/pengjun/kaserver/net/KaServer.java
@@ -1,47 +1,31 @@
 package com.pengjun.kaserver.net;
 
-import java.net.InetSocketAddress;
-import java.util.concurrent.Executors;
-
-import org.jboss.netty.bootstrap.ServerBootstrap;
-import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
-
 import com.pengjun.kaserver.utils.KaServerConstants;
+import com.pengjun.net.BaseNettyServer;
 
-public class KaServer {
+public class KaServer extends BaseNettyServer {
+	private final static int port = 8000;
 
-	private final int port;
-
-	public KaServer(int port) {
-		this.port = port;
+	private KaServer(int port) {
+		super(port);
 	}
 
-	public void run() {
+	private static KaServer kaClient;
 
-		KaServerConstants.serviceLogger.info("Server start");
-		ServerBootstrap bootstrap = new ServerBootstrap(new NioServerSocketChannelFactory(
-				Executors.newCachedThreadPool(), Executors.newCachedThreadPool()));
+	public static KaServer getInstance() {
+		if (kaClient == null) {
+			kaClient = new KaServer(port);
+		}
+		return kaClient;
+	}
 
-		bootstrap.setPipelineFactory(new KaServerPipelineFactory());
-
-		bootstrap.bind(new InetSocketAddress(port));
-
+	public void start() {
+		super.start(new KaServerPipelineFactory());
 	}
 
 	public static void main(String[] args) throws Exception {
 
 		KaServerConstants.serviceLogger.info("main start");
-
-		// UserInfo userInfo = new UserInfo();
-		// userInfo.setCreateDate("");
-		// userInfo.setUpdateTime("");
-		// userInfo.setUserName("pj");
-		// userInfo.setPassword("123");
-		//
-		// UserInfoDao.insert(userInfo);
-
-		// LoggerUtils.serviceLogger.info("UserInfoDao.loginVerify " +
-		// UserInfoDao.loginVerify("pj", "123"));
-		new KaServer(8000).run();
+		KaServer.getInstance().start();
 	}
 }
